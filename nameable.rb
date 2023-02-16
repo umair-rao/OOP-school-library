345fda4bccfc952bcf653e8c57a180527e315276
umair-rao <umairmuqeem@gmail.com>
2023-02-16 18:47:13 +0800

    remove linters error

--- nameable.rb
+++ nameable.rb
@@ -1,4 +1,4 @@
-#create nameable class with correct_name that can be alter by the decorator
+# create nameable class with correct_name that can be alter by the decorator
 class Nameable
   def correct_name
     raise NotImplementedError, 'You must implement the correct_name method'
