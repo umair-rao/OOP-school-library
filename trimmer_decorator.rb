26f97a94b05c583103c748973c28ceb0784a09ca
umair-rao <umairmuqeem@gmail.com>
2023-02-16 18:02:21 +0800

    remove linters error

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,8 +1,7 @@
 class TrimmerDecorator < Decorator
-	def correct_name
-	  if @nameable.correct_name.length > 10
-		@nameable.correct_name[0, 10]
-  
+  def correct_name
+  if @nameable.correct_name.length > 10
+  @nameable.correct_name[0, 10]
 	  else
 		@nameable.correct_name
 	  end
