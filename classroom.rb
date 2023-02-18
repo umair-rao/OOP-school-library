663c41fc1bf647e460784bb1f5e3930ab7269157
umair-rao <umairmuqeem@gmail.com>
2023-02-18 20:11:12 +0500

    remove linters error

--- classroom.rb
+++ classroom.rb
@@ -3,14 +3,14 @@ class Classroom
   attr_reader :student
 
   def initialize(label)
-     @label = label
-     @student = []
+    @label = label
+    @student = []
   end
 
-#add student manage the relationship between the classroom and student
+# add student manage the relationship between the classroom and student
 
   def add_student(student)
-     @student.push(student)
-     student.manage_classroom = self
+    @student.push(student)
+    student.manage_classroom = self
   end
 end
