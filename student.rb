870a00ff671c337607c45d96a0ce436b0d5f998e
umair-rao <umairmuqeem@gmail.com>
2023-02-19 00:29:41 +0500

    Remove error

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknow', parent_permission = true,classroom)
+  def initialize(age, name = 'Unknow', parent_permission = true, classroom)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
