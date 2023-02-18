d3d2719cb6beeabff4ff8e293fb7524145f50f1f
umair-rao <umairmuqeem@gmail.com>
2023-02-18 23:28:28 +0500

    Remove error

--- app.rb
+++ app.rb
@@ -105,7 +105,6 @@ class App
     puts 'Rentals:'
     @rentals.each do |rental|
       puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}" if rental.person.id == id
-      end
     end
   end
 end
